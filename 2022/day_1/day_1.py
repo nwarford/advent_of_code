49f2d661fe3d0da7d908bd97933c11a1f555756b
Noel Warford <noel.warford95@gmail.com>
2022-12-09 10:09:52 -0500

    aoc 2022 day 1

--- 2022/day_1/day_1.py
+++ 2022/day_1/day_1.py
@@ -1,5 +1,62 @@
 
 
-print("test")
+# inputfile = open('test.txt', 'r')
+inputfile = open('input.txt', 'r')
+lines = inputfile.read().splitlines()
 
-print("second test")
+# takes input, divides elves into arrays of each elf's individual inventory
+def createElves() :
+    elflist = []
+    currElf = []
+    for line in lines:
+        if line == "" :
+            elflist.append(currElf)
+            currElf = []
+            next
+        else :
+            currElf.append(int(line))
+
+    elflist.append(currElf)
+    return elflist
+
+
+elves = createElves()
+
+# part 1
+max = 0
+
+for elf in elves :
+    currSum = 0
+    for inv in elf :
+        currSum += inv
+
+    if currSum > max :
+        max = currSum
+
+print("Maximum inventory: %d" % max)
+
+top_max = 0
+second_max = 0
+third_max = 0
+
+for elf in elves :
+    currSum = 0
+    for inv in elf :
+        currSum += inv
+
+    if currSum > third_max :
+        if currSum > second_max :
+            if currSum > top_max :
+                third_max = second_max
+                second_max = top_max
+                top_max = currSum
+            else :
+                third_max = second_max
+                second_max = currSum
+        else :
+            third_max = currSum
+
+print("Top max: %d" % top_max)
+print("Second max: %d" % second_max)
+print("Third max: %d" % third_max)
+print("Total: %d" % (top_max + second_max + third_max))
